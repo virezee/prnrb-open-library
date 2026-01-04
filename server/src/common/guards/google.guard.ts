56776a0d17ab6a2de1296515a322cfded72e5952
Zee <56460839+virezee@users.noreply.github.com>
2026-01-04 19:58:55 +0700

    .

--- server/src/common/guards/google.guard.ts
+++ server/src/common/guards/google.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable, type ExecutionContext } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
-import type { User } from '@type/auth/user.d.ts'
 
 @Injectable()
 export class GoogleAuthGuard extends AuthGuard('google') {
@@ -14,17 +13,5 @@ export class GoogleAuthGuard extends AuthGuard('google') {
             state,
             prompt: 'select_account'
         }
-    }
-    override handleRequest<T = User>(err: Error, user: T, _: never, context: ExecutionContext): T | undefined | never {
-        const res = context.switchToHttp().getResponse()
-        if (err) {
-            return res.send(`
-                <script>
-                    window.opener.postMessage({ message: '${err.message}' }, 'http://${process.env['DOMAIN']}:${process.env['CLIENT_PORT']}')
-                    window.close()
-                </script>
-            `)
-        }
-        return user
     }
 }
